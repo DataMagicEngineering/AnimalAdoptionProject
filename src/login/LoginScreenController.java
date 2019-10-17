5921004ef80a18c10582bd17d247c843d44d0e31
Zachary Maroney <zamaroney7986@eagle.fgcu.edu>
2019-10-17 16:10:53 -0400

    implemented database into login screen. swithces to the mainScreen scene.

--- src/login/LoginScreenController.java
+++ src/login/LoginScreenController.java
@@ -2,41 +2,56 @@ package login;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javax.xml.crypto.Data;
+import main.Database;
 
 public class LoginScreenController {
 
-  @FXML private AnchorPane backPane;
+  @FXML
+  private AnchorPane backPane;
 
-  @FXML private TextField usernameText;
+  @FXML
+  private TextField usernameText;
 
-  @FXML private TextField passwordText;
+  @FXML
+  private PasswordField passwordText;
 
-  @FXML private Button loginButton;
+  @FXML
+  private Button loginButton;
 
-  @FXML private Label errorLabel;
+  @FXML
+  private Label errorLabel;
 
   @FXML
   private Label logginLabel;
 
-  /** @param event pressing the login button */
+  /**
+   * @param event pressing the login button
+   */
   @FXML
-  void pressedLogin(ActionEvent event) {
-    /*
-    insert code that will take in the username and password and run it through
-    the database to make sure there is a connection
-     */
-    if (!usernameText.getText().equals("") && !passwordText.getText().equals("")) {
-      errorLabel.setVisible(false);
-      logginLabel.setVisible(true);
-    }
-    else {
-      logginLabel.setVisible(false);
+  void pressedLogin(ActionEvent event) throws Exception {
+    Database database = Database.get();
+    if (database.loginUser(usernameText.getText(), passwordText.getText()) == null) {
       errorLabel.setVisible(true);
+    } else {
+      errorLabel.setVisible(false);
+      Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      Parent root = FXMLLoader
+          .load(getClass().getResource("../mainScreen/AnimalAdoptMainScreen.fxml"));
+      primaryStage.setTitle("Main Screen");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.show();
     }
-    System.out.println("User logged in!");
   }
 }
