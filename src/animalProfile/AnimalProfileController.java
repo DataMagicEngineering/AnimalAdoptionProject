57c9cf7ae594022fdf66b5bf1d632fa70430eb42
Armeg <Armeg@DESKTOP-O6RQQNN>
2019-10-18 18:47:03 -0400

    Added some button functionality to the Animal profile

--- src/animalProfile/AnimalProfileController.java
+++ src/animalProfile/AnimalProfileController.java
@@ -1,56 +1,100 @@
 package animalProfile;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class AnimalProfileController {
 
   @FXML
-  private Label animNameLb;
+  private Text animNameText;
 
   @FXML
-  private Label animSpeciesLb;
+  private Text animSpeciesText;
 
   @FXML
-  private Label animBreedLb;
+  private Text animBreedText;
 
   @FXML
-  private Label AnimGenderLb;
+  private Text animGenderText;
 
   @FXML
-  private Label animIDLb;
+  private Text animIDText;
 
   @FXML
-  private Label animalColorLbl;
+  private Text animBioText;
 
   @FXML
-  private Label animWeightLb;
+  private Text animColorText;
 
   @FXML
-  private Label animHieghtLb;
+  private Text animWeightText;
 
   @FXML
-  private Label animAgressLb;
+  private Text animHeightText;
 
   @FXML
-  private TextArea BioText;
+  private Text animAggressText;
 
   @FXML
-  private Label animSpecLb11;
+  private Text animBathroomText;
 
   @FXML
-  private Label animeAdoptedLb;
+  private Text animServiceText;
 
   @FXML
-  private Label animDateArrivedLb;
+  private Text animAdoptedText;
 
   @FXML
-  private Label animDateAdopLb;
+  private Text animDateArrivedText;
 
   @FXML
-  private Label animBDayLb;
+  private Text animDateAdoptedText;
 
   @FXML
-  private Label animBathLb;
+  private Text animBirthdayText;
+
+  @FXML
+  private Button editAnimalBtn;
+
+  @FXML
+  private Button returnBtn;
+
+  @FXML
+  private Button adoptBtn;
+
+  @FXML
+  void goToAdoptionPage(ActionEvent event) {
+
+  }
+
+  @FXML
+  void goToEdit(ActionEvent event) {
+
+  }
+
+  /**
+   * returns the user to the menu scene when the button is pressed.
+   *
+   * @param event User clicks/presses enter on the button
+   * @throws Exception when the scene is not located at the same file location as stated
+   */
+  @FXML
+  void returnToMenu(ActionEvent event) throws Exception {
+    Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    Parent root = FXMLLoader
+        .load(getClass().getResource("../mainScreen/AnimalAdoptMainScreen.fxml"));
+    primaryStage.setTitle("Main Screen");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+  }
+
 }
