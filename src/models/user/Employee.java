42d8342d81cfd620fef1af224229e2a2b0d0fa02
reltaher <43248170+reltaher@users.noreply.github.com>
2019-10-19 14:02:52 -0400

    Added JavaDoc comments to the Employee Class, Volunteer Class, and User Class.

--- src/models/user/Employee.java
+++ src/models/user/Employee.java
@@ -3,6 +3,9 @@ package models.user;
 public class Employee extends User {
   private int salary;
 
+  /**
+   * Default Constructor for an Employee object, which sets their privilege level to administration.
+   */
   public Employee() {
 
     privileges = AuthorizationLevel.ADMINISTRATION;
--- src/models/user/User.java
+++ src/models/user/User.java
@@ -14,58 +14,114 @@ public abstract class User {
   protected Instant dateOfBirth;
   protected AuthorizationLevel privileges;
 
+  /**
+   * Accessor for id
+   * @return id, an int field in the User class.
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * Mutator for id
+   * @param id the int variable that the id field is assigned to.
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Accessor for username
+   * @return username, a String field in the User class.
+   */
   public String getUsername() {
     return username;
   }
 
+  /**
+   * Mutator for username
+   * @param username the String variable that the username field is assigned to.
+   */
   public void setUsername(String username) {
     this.username = username;
   }
 
+  /**
+   * Accessor for password
+   * @return password, a String field in the User class.
+   */
   public String getPassword() {
     return password;
   }
 
+  /**
+   * Mutator for password
+   * @param password the String variable that the password field is assigned to.
+   */
   public void setPassword(String password) {
     this.password = password;
   }
 
+  /**
+   * Accessor for firstName
+   * @return firstName, a String field in the User class.
+   */
   public String getFirstName() {
     return firstName;
   }
 
+  /**
+   * Mutator for firstName
+   * @param firstName the String variable that the firstName field is assigned to.
+   */
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
 
+  /**
+   * Accessor for lastName
+   * @return lastName, a String field in the User class.
+   */
   public String getLastName() {
     return lastName;
   }
 
+  /**
+   * Mutator for lastName
+   * @param lastName the String variable that the lastName field is assigned to.
+   */
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
 
+  /**
+   * Accessor for dateOfBirth
+   * @return dateOfBirth, an Instant field in the User class.
+   */
   public Instant getDateOfBirth() {
     return dateOfBirth;
   }
 
+  /**
+   * Mutator for dateOfBirth
+   * @param dateOfBirth the Instant variable that the dateOfBirth field is assigned to.
+   */
   public void setDateOfBirth(Instant dateOfBirth) {
     this.dateOfBirth = dateOfBirth;
   }
 
+  /**
+   * Accessor for privileges
+   * @return privileges, an AuthorizationLevel enum field in the User class.
+   */
   public AuthorizationLevel getPrivileges() {
     return privileges;
   }
 
+  /**
+   * Mutator for privileges
+   * @param privileges the AuthorizationLevel enum variable that the privileges field is assigned to.
+   */
   public void setPrivileges(AuthorizationLevel privileges) {
     this.privileges = privileges;
   }
