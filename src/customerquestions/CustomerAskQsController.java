f52098fc97d7e6bfedf4431221db599c1cc113b7
emilyrschwarz <emilyrschwarz99@gmail.com>
2019-11-03 17:14:52 -0500

    Added customer "ask a question" files

---
+++ src/customerquestions/CustomerAskQsController.java
@@ -0,0 +1,18 @@
+package customerquestions;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class CustomerAskQsController {
+
+  @FXML
+  private Button customerQsBackBtn;
+
+  @FXML
+  private TextArea customerQsTxtArea;
+
+  @FXML
+  private Button customerSubmitQsBtn;
+
+}
