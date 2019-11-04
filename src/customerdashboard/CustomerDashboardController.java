026d890f39a7396951714fe648c10e072cef7968
cobol <cobolongo22@gmail.com>
2019-11-04 18:20:20 -0500

    added fxml id for buttons

--- src/customerdashboard/CustomerDashboardController.java
+++ src/customerdashboard/CustomerDashboardController.java
@@ -2,13 +2,30 @@ package customerdashboard;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class CustomerDashboardController {
+
+  @FXML
+  private Button customerViewAnimalsBtn;
+
+  @FXML
+  private Button customerViewEventsBtn;
+
+  @FXML
+  private Button askQuestionBtn;
+
+  @FXML
+  private Button customerLogOutBtn;
+
+  @FXML
+  private Button applyToVolunteerBtn;
 
   public void goToAnimalScreen(ActionEvent actionEvent) throws IOException {
     Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
