7a23cc42e1a931d243555598e2d1271255530998
emilyrschwarz <emilyrschwarz99@gmail.com>
2019-11-04 18:17:14 -0500

    Merge branch 'master' of C:\Users\ersch\OneDrive - Florida Gulf Coast University\AnimalAdoptionProject with conflicts.

--- src/customerdashboard/CustomerDashboardController.java
+++ src/customerdashboard/CustomerDashboardController.java
@@ -1,30 +1,20 @@
 package customerdashboard;
 
-import java.io.IOException;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 public class CustomerDashboardController {
 
-  public void goToAnimalScreen(ActionEvent actionEvent) throws IOException {
-    Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-    Parent root = FXMLLoader
-        .load(getClass().getResource("../animalList/AnimalList.fxml"));
-    primaryStage.setTitle("Animal Screen");
-    primaryStage.setScene(new Scene(root));
-    primaryStage.show();
-  }
+  @FXML
+  private Button customerViewAnimalsBtn;
 
-  public void goToEventsScreen(ActionEvent actionEvent) throws IOException {
-    Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-    Parent root = FXMLLoader
-        .load(getClass().getResource("../newevent/NewEvent.fxml"));
-    primaryStage.setTitle("Events");
-    primaryStage.setScene(new Scene(root));
-    primaryStage.show();
-  }
+  @FXML
+  private Button customerViewEventsBtn;
+
+  @FXML
+  private Button askQuestionBtn;
+
+  @FXML
+  private Button customerLogOutBtn;
+
+  @FXML
+  private Button applyToVolunteerBtn;
+
 }
