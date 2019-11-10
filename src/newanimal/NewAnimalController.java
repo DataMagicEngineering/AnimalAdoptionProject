6aa339403afaf4d6c0f032744b0ed268ef3ea57a
Armeg <Armeg@DESKTOP-O6RQQNN>
2019-11-09 19:28:49 -0500

    Can Edit Animal

--- src/newanimal/NewAnimalController.java
+++ src/newanimal/NewAnimalController.java
@@ -2,6 +2,7 @@ package newanimal;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.chrono.Chronology;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javafx.collections.FXCollections;
@@ -10,15 +11,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javax.xml.crypto.Data;
 import main.Database;
 import models.animal.Animal;
 import models.animal.Color;
@@ -44,6 +48,7 @@ public class NewAnimalController {
   public ToggleButton serviceTrainingToggle;
   public DatePicker dateOfBirth;
   public TextArea descriptionInput;
+  public Button createBtn;
 
   public void initialize() {
     dateOfBirth.setValue(LocalDate.now());
@@ -58,6 +63,7 @@ public class NewAnimalController {
 
     currentAggression.setText(Color.values()[0].toString());
     colorsList.setItems(FXCollections.observableList(Arrays.asList(Color.values())));
+    colorsList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
     serviceTrainingToggle.selectedProperty().addListener((observableValue, old, isToggled) -> {
       serviceTrainingToggle.setText(isToggled ? "Yes" : "No");
@@ -66,6 +72,8 @@ public class NewAnimalController {
     genderToggle.selectedProperty().addListener((observableValue, oldVal, newVal) -> {
       genderToggle.setText(newVal ? "Male" : "Female");
     });
+
+    editAnimal();
   }
 
   public void cancelCreation(ActionEvent event) throws Exception {
@@ -74,14 +82,43 @@ public class NewAnimalController {
     primaryStage.setTitle("Animal List");
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
+
+    Database.setCurrentAnimal(null);
   }
 
-  public void createAnimal(ActionEvent actionEvent) {
+  public void editAnimal() {
+    Animal animal = Database.getCurrentAnimal();
+    if (animal != null) {
+      nameInput.setText(animal.getName());
+      speciesInput.setText(animal.getSpecies());
+      breedsInput.setText(animal.getBreedString().replace("|", ","));
+      heightInput.setText(String.valueOf(animal.getHeight()));
+      weightInput.setText(String.valueOf(animal.getWeight()));
+      descriptionInput.setText(animal.getDescription());
+      tricksInput.setText(animal.getTricks().toString().replace("{", "").replace("}", ""));
+      for (Color colors : animal.getColors()) {
+        colorsList.getSelectionModel().select(colors);
+      }
+      dateOfBirth.setChronology(Chronology.from(animal.getDateOfBirth()));
+      serviceTrainingToggle.setSelected(animal.isServiceTrained());
+      bathroomTrainingBox.getSelectionModel().select(animal.getBathroomTraining());
+      aggressionSlider.setValue(animal.getAggression().ordinal());
+
+      createBtn.setText("Edit");
+    }
+
+  }
+
+  public void createAnimal(ActionEvent actionEvent) throws Exception {
     if (!validInput()) {
       return;
     }
 
     Animal animal = new Animal();
+
+    if (Database.getCurrentAnimal() != null) {
+      animal = Database.getCurrentAnimal();
+    }
 
     animal.setName(nameInput.getText());
     animal.setSpecies(speciesInput.getText());
@@ -99,7 +136,19 @@ public class NewAnimalController {
     animal.setVaccines(new ArrayList<>()); // Temp empty list
     animal.setAggression(Color.values()[(int) aggressionSlider.getValue()]);
 
-    database.addAnimal(animal);
+    if (Database.getCurrentAnimal() != null) {
+      database.editAnimal(animal);
+    } else {
+      database.addAnimal(animal);
+    }
+
+    Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    Parent root = FXMLLoader.load(getClass().getResource("../animalList/AnimalList.fxml"));
+    primaryStage.setTitle("Animal List");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+
+    Database.setCurrentAnimal(null);
   }
 
   private boolean validInput() {
