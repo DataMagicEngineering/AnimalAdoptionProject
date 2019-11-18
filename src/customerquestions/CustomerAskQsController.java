fdac56c4aa9f26202fe46cd8044154a2b4ec6535
reltaher <43248170+reltaher@users.noreply.github.com>
2019-11-18 12:38:30 -0500

    CustomerAskQsController: Added Functionality to the "Submit Question" button. The button will now process the question in a question object, send the information to the Question table in the database, and switches the customers scene back to the dashboard (temporary, a confirmation page will be made soon and the scene will switch to that).

--- src/customerquestions/CustomerAskQsController.java
+++ src/customerquestions/CustomerAskQsController.java
@@ -33,6 +33,12 @@ public class CustomerAskQsController {
   @FXML
   private Button customerSubmitQsBtn;
 
+  /**
+   * Method that switches scenes to the customer dashboard.
+   * @param event gets the Source, Scene, and Window of the selected scene, and is casted to a
+   *              Node.
+   * @throws IOException since the method has a chance to contain an IOException.
+   */
   @FXML
   void onActionGoBack(ActionEvent event) throws IOException {
     Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -43,8 +49,30 @@ public class CustomerAskQsController {
     primaryStage.show();
   }
 
+  /**
+   * Method that processes an asked question, sends the information to the database, and informs the
+   * user that the question was successfully submitted.
+   * @param event gets the Source, Scene, and Window of the selected scene, and is casted to a
+   *              Node.
+   * @throws IOException since the method has a chance to contain an IOException.
+   */
   @FXML
-  void onActionSubmitQuestion(ActionEvent event) {
-
+  void onActionSubmitQuestion(ActionEvent event) throws IOException {
+    Question customerQuestion = new Question();
+    // Gets the ID of the current user
+    customerQuestion.setUserId(Database.getCurrentUser().getId());
+    // Sets the question to the text in the TextArea.
+    customerQuestion.setQuestion(customerQsTxtArea.getText());
+    // Sets the boolean variable to decide if the question was answered to false.
+    customerQuestion.setAnswered(false);
+    // Process the asked question and sends the information to the database.
+    Database.get().askQuestion(customerQuestion);
+    // Switches scenes to the confirmation page. (CURRENTLY SWITCHES BACK TO CUSTOMER DASHBOARD)
+    Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    Parent root = FXMLLoader
+        .load(getClass().getResource("../customerdashboard/CustomerDashboard.fxml"));
+    primaryStage.setTitle("Main Screen");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
   }
 }
