c8651c9f16d686c4cc6cdf9f7d55d2aeadf67a96
reltaher <43248170+reltaher@users.noreply.github.com>
2019-11-27 18:12:56 -0500

    VolunteerApplicationWithUser: Added JavaDoc comments.

--- src/models/application/VolunteerApplicationWithUser.java
+++ src/models/application/VolunteerApplicationWithUser.java
@@ -5,23 +5,43 @@ import java.time.format.FormatStyle;
 import models.event.Event;
 import models.user.User;
 
+/**
+ * The class that checks whether a User's volunteer application has been accepted or rejected.
+ */
 public class VolunteerApplicationWithUser {
   private User user;
   private VolunteerApplication application;
 
+  /**
+   * Constructor which is used for creating a new object for the class.
+   * @param user A user instance.
+   * @param application A VolunteerApplication instance.
+   */
   public VolunteerApplicationWithUser(User user, VolunteerApplication application) {
     this.user = user;
     this.application = application;
   }
 
+  /**
+   * Accessor for user.
+   * @return user, a field of type User.
+   */
   public User getUser() {
     return user;
   }
 
+  /**
+   * Accessor for application.
+   * @return application, a field of type VolunteerApplication.
+   */
   public VolunteerApplication getApplication() {
     return application;
   }
 
+  /**
+   * A method that returns an application's information as a String.
+   * @return the user's first name, last name, request date, and approved line, formatted as a String.
+   */
   @Override
   public String toString() {
     String format =
