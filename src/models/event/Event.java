af709d20136fed3a8e3249b7279d2b78551ede53
reltaher <43248170+reltaher@users.noreply.github.com>
2019-11-27 20:03:06 -0500

    Event Class: Added JavaDoc Comments.

--- src/models/event/Event.java
+++ src/models/event/Event.java
@@ -5,103 +5,202 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+
 import models.user.AuthorizationLevel;
 
+/**
+ * The Event class contains the information about an event in the program, which includes the type of event, when it was
+ * published, the target audience, and the description.
+ */
 public class Event {
 
-  public static ZoneId EST = ZoneOffset.of("EST", ZoneId.SHORT_IDS);
-  private int id;
-  private String name;
+    public static ZoneId EST = ZoneOffset.of("EST", ZoneId.SHORT_IDS);
+    private int id;
+    private String name;
 
-  // Id of the user who originally created this event
-  private int eventCreatorId;
-  private Instant date;
-  private boolean isPublished;
-  private AuthorizationLevel targetAudience;
-  private String description;
+    // Id of the user who originally created this event
+    private int eventCreatorId;
+    private Instant date;
+    private boolean isPublished;
+    private AuthorizationLevel targetAudience;
+    private String description;
 
-  public Event(int id, String name, int eventCreatorId, Instant date, boolean isPublished,
-      AuthorizationLevel targetAudience, String description) {
-    this.id = id;
-    this.name = name;
-    this.eventCreatorId = eventCreatorId;
-    this.date = date;
-    this.isPublished = isPublished;
-    this.targetAudience = targetAudience;
-    this.description = description;
-  }
+    /**
+     * The Constructor used for creating Event objects with known information.
+     *
+     * @param id             an int variable that is used to assign the id field to.
+     * @param name           a String variable that is used to assign the name field to.
+     * @param eventCreatorId an int variable that is used to assign the eventCreatorId field to.
+     * @param date           an Instant instance variable that is used to assign the date field to.
+     * @param isPublished    a boolean variable that is used to assign the isPublished field to.
+     * @param targetAudience an AuthorizationLevel instance variable that is used to assign the targetAudience field to.
+     * @param description    a String variable that is used to assign the description field to.
+     */
+    public Event(int id, String name, int eventCreatorId, Instant date, boolean isPublished,
+                 AuthorizationLevel targetAudience, String description) {
+        this.id = id;
+        this.name = name;
+        this.eventCreatorId = eventCreatorId;
+        this.date = date;
+        this.isPublished = isPublished;
+        this.targetAudience = targetAudience;
+        this.description = description;
+    }
 
-  public Event() {
-  }
+    /**
+     * Default constructor for Event.
+     */
+    public Event() {
+    }
 
-  public int getEventCreatorId() {
-    return eventCreatorId;
-  }
+    /**
+     * Accessor for eventCreatorId.
+     *
+     * @return eventCreatorId, a field variable of type int.
+     */
+    public int getEventCreatorId() {
+        return eventCreatorId;
+    }
 
-  public void setEventCreatorId(int eventCreatorId) {
-    this.eventCreatorId = eventCreatorId;
-  }
+    /**
+     * Mutator for eventCreatorId.
+     *
+     * @param eventCreatorId
+     */
+    public void setEventCreatorId(int eventCreatorId) {
+        this.eventCreatorId = eventCreatorId;
+    }
 
-  public int getId() {
-    return id;
-  }
+    /**
+     * Accessor for id.
+     *
+     * @return id, a field variable of type int.
+     */
+    public int getId() {
+        return id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    /**
+     * Mutator for id.
+     *
+     * @param id the variable that the id field is assigned to.
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    /**
+     * Accessor for name.
+     *
+     * @return name, a field variable of type String.
+     */
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    /**
+     * Mutator for name.
+     *
+     * @param name the variable that the name field is assigned to.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public Instant getDate() {
-    return date;
-  }
+    /**
+     * Accessor for date.
+     *
+     * @return date, a field variable of type Instant.
+     */
+    public Instant getDate() {
+        return date;
+    }
 
-  public void setDate(Instant date) {
-    this.date = date;
-  }
+    /**
+     * Mutator for date.
+     *
+     * @param date the variable that the date field is assigned to.
+     */
+    public void setDate(Instant date) {
+        this.date = date;
+    }
 
-  public boolean isPublished() {
-    return isPublished;
-  }
+    /**
+     * Accessor for isPublished.
+     *
+     * @return isPublished, a field variable of type boolean.
+     */
+    public boolean isPublished() {
+        return isPublished;
+    }
 
-  public void setPublished(boolean published) {
-    isPublished = published;
-  }
+    /**
+     * Mutator for isPublished.
+     *
+     * @param published the variable that the isPublished field is assigned to.
+     */
+    public void setPublished(boolean published) {
+        isPublished = published;
+    }
 
-  public AuthorizationLevel getTargetAudience() {
-    return targetAudience;
-  }
+    /**
+     * Accessor for targetAudience.
+     *
+     * @return targetAudience, a field variable of type AuthorizationLevel.
+     */
+    public AuthorizationLevel getTargetAudience() {
+        return targetAudience;
+    }
 
-  public void setTargetAudience(AuthorizationLevel targetAudience) {
-    this.targetAudience = targetAudience;
-  }
+    /**
+     * Mutator for targetAudience.
+     *
+     * @param targetAudience the variable that the targetAudience field is assigned to.
+     */
+    public void setTargetAudience(AuthorizationLevel targetAudience) {
+        this.targetAudience = targetAudience;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    /**
+     * Accessor for description.
+     *
+     * @return description, a field variable of type String.
+     */
+    public String getDescription() {
+        return description;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    /**
+     * Mutator for description.
+     *
+     * @param description the variable that the description field is assigned to.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public LocalDateTime getDateTime() {
-    return date.atZone(EST).toLocalDateTime();
-  }
+    /**
+     * Accessor for date.
+     *
+     * @return date, a field variable of type LocalDateTime, converted to local time.
+     */
+    public LocalDateTime getDateTime() {
+        return date.atZone(EST).toLocalDateTime();
+    }
 
-  @Override
-  public String toString() {
-    String format =
-        "Event Name: %s\n"
-        + "Date: %s\n"
-        + "Description: %s";
-    DateTimeFormatter formatDate;
-    formatDate = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy @ hh:mm a").withZone(EST);
-    return String.format(format, getName(), formatDate.format(getDate()), getDescription());
-  }
+    /**
+     * A toString method that displays the Event information
+     *
+     * @return The Event's name, date, and description, formatted as a String.
+     */
+    @Override
+    public String toString() {
+        String format =
+                "Event Name: %s\n"
+                        + "Date: %s\n"
+                        + "Description: %s";
+        DateTimeFormatter formatDate;
+        formatDate = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy @ hh:mm a").withZone(EST);
+        return String.format(format, getName(), formatDate.format(getDate()), getDescription());
+    }
 }
